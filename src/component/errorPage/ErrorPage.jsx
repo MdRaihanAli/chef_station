1443c96f4afd575b0cc58d0f72d84b642e921862
ali <raihanraju43@gmail.com>
2023-05-04 16:46:53 +0400

    404 page image add

--- src/component/errorPage/ErrorPage.jsx
+++ src/component/errorPage/ErrorPage.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 import { FaBan } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
+import bg_img from '../../assets/err.webp'
+import Header from '../Header/Header';
 
 function ErrorPage() {
+
+
+
   return (
-    <div className='text-center py-5'>
-        <h1 className='display-5'> <FaBan /> </h1>
-        <h2>This Pase Is Not Found</h2>
-        <Link to='/'>Goto Home</Link>
+    <div fluid
+      style={{
+        backgroundImage: `url(${bg_img})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+      className='text-center '>
+      <div className='bg-dark'>
+        <Header></Header>
+      </div>
+      {/* <h1 className='display-5'> <FaBan /> </h1>
+        
+        <Link to='/'> <button className='btn btn-primary'>Goto Home</button></Link> */}
     </div>
   )
 }
